ece2970d690c857803663b7d9c8ad47f3e0f0c1a
Peter van Heusden <pvh@sanbi.ac.za>
2014-09-18 13:12:56 +0200

    * Added code to run exonerate wrapper via queue (using DRMAA)

--- genblastA_process_ruffus.py
+++ genblastA_process_ruffus.py
@@ -7,10 +7,13 @@ import argparse
 import glob
 from subprocess import Popen, PIPE
 import shlex
+import re
+import logging
 
 from Bio import SeqIO
 from ruffus import *
-from genblastA_to_gff3 import genblastA_process
+from ruffus.drmaa_wrapper import run_job, error_drmaa_job
+from genblastA_to_gff.genblastA_to_gff3 import genblastA_process
 # import Environment Modules stuff
 MODULES_KEY = 'MODULESHOME'
 if MODULES_KEY in os.environ:
@@ -23,9 +26,31 @@ parser = argparse.ArgumentParser(description='Use Ruffus to process .out files f
 parser.add_argument('--input_pattern', '-I', default='.out')
 parser.add_argument('--working_directory', '-W', default='.')
 parser.add_argument('--num_threads', '-N', type=int, default=1)
+parser.add_argument('--run_local', '-L', action='store_true', default=False, help='Force to run jobs locally, not on cluster')
+parser.add_argument('--scripts_dir', '-S', default='/cip0/research/pvh/software/sanbi-scripts', help='Location of pvh sanbi-scripts')
+parser.add_argument('--modules_home', '-M', default='/cip0/software/x86_64/modules/Modules/3.2.9', help='Value of Environment Modules MODULESHOME env variable')
+parser.add_argument('--query_type', default='dna', choices=['dna','protein'], help='exonerate query type (dna or protein)')
+parser.add_argument('--debug', action='store_true', default=False, help='Set log level to DEBUG')
 parser.add_argument('genome_filename', help='FASTA format genome file, must end in .fa or .fasta')
 parser.add_argument('query_filename', help='FASTA format file of query sequences, must end in .fa or .fasta')
 args = parser.parse_args()
+
+if args.debug:
+	logging.basicConfig(level=logging.DEBUG)
+else:
+	logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('genblastA_process_ruffus')
+
+drmaa_session = None
+if not args.run_local:
+	try:
+		import drmaa
+		drmaa_session = drmaa.Session()
+		drmaa_session.initialize()
+	except ImportError:
+		sys.stderr.write('Failed to import drmaa module, must run locally')
+		args.run_local = True
+
 
 FASTA_RE=r'\.(fa|fasta)$'
 
@@ -66,4 +91,29 @@ def make_twobit(input_file, output_file):
 def make_index(input_file, output_file):
 	SeqIO.index_db(output_file, input_file, 'fasta')
 
+FASTA_RE_COMPILED = re.compile(FASTA_RE)
+PATH_val = os.environ['PATH'] + ':' + os.path.join(args.scripts_dir,'run_est_mapping')
+PYTHONPATH_val = os.path.join(args.scripts_dir, 'lib')
+@transform(genblastA_to_gff3,
+	       suffix('.genblastA.gff3'),
+	       '.exonerate.gff3',
+	       args.genome_filename, args.query_filename)
+def run_exonerate(input_file, output_file, genome_filename, query_filename):
+	twobit_filename = FASTA_RE_COMPILED.sub('.2bit', genome_filename)
+	job_name = input_file.replace('.genblastA.gff3', '.sge')
+	job = 'run_est_mapping.py --query_type {}'.format(args.query_type)
+	job += ' --upstream 3000 --downstream 3000 --mapper exonerate --save_mapper_output --augustus_hints'
+	job += ' {} {} {} {}'.format(query_filename, input_file, twobit_filename, output_file)
+	job_queue = 'all.q'
+	job_env = dict(PATH=PATH_val, PYTHONPATH=PYTHONPATH_val)
+	if not args.run_local:
+		job_env['MODULESHOME'] = args.modules_home
+	run_job(job, job_name=job_name, job_other_options='-q {}'.format(job_queue),
+		    job_environment=job_env, drmaa_session=drmaa_session, 
+		    working_directory=args.working_directory,
+		    run_locally=args.run_local, logger=logger)
+
 pipeline_run(multiprocess=args.num_threads)
+
+if not args.run_local:
+	drmaa_session.exit()
